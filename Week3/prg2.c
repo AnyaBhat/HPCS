fcd62814f1db225db1df37c0d5c47232feaec03c
Ananya Bhat <anyabhat18@gmail.com>
2024-08-29 09:41:07 +0530

    Update prg2.c

--- Week3/prg2.c
+++ Week3/prg2.c
@@ -16,21 +16,29 @@ int main(int argc, char *argv[]) {
     if (rank == 0) {
         printf("Enter the number of elements (m) : ");
         scanf("%d", &m);
+
+        // Calculate total number of elements
         M = m * size;
+
+        printf("Total number of elements: %d\n", M);
+
+        // Allocate memory for the entire array
         array = (int*)malloc(M * sizeof(int));
         if (array == NULL) {
-            perror("Unable to allocate memory");
+            perror("Unable to allocate memory for array");
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
-        printf("Enter the elements of the array:");
+
+        printf("Enter the elements:\n");
         for (int i = 0; i < M; i++) {
             scanf("%d", &array[i]);
         }
     }
 
+    // Allocate memory for subarray
     subarr = (int*)malloc((M / size) * sizeof(int));
     if (subarr == NULL) {
-        perror("Unable to allocate memory");
+        perror("Unable to allocate memory for subarray");
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
@@ -49,7 +57,7 @@ int main(int argc, char *argv[]) {
     if (rank == 0) {
         arr = (int*)malloc(size * sizeof(int));
         if (arr == NULL) {
-            perror("Unable to allocate memory");
+            perror("Unable to allocate memory for arr");
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
     }
